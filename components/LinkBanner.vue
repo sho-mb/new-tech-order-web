14088153f130c63b9dde82ca473566459c977544
Kisho (Sho) Tamura <sho@mango-byte.com>
2024-05-29 14:43:22 +0700

    feat: add banner and swiper

---
+++ components/LinkBanner.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <NuxtLink to="https://www.instagram.com/p/C68-rq2hlKF/?img_index=1">
+      <img
+        src="/img/banner.png"
+        class="mt-[100px]"
+      >
+    </NuxtLink>
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+
+</style>
---
+++ components/WorksSection.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="relative top-0">
+    <h2 class="font-montserrat text-4xl">
+      WORKS
+    </h2>
+    <p class="w-md">
+      Our influencer marketing service harnesses the power of influencers to convey the allure of products and services, attracting customer interest.
+    </p>
+
+    <div class="mt-10">
+      <WorksSwiper :works="works" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import 'swiper/css'
+import 'swiper/css/navigation'
+
+const works = [
+  {
+    id: 1,
+    category: 'Influencer marketing',
+    content: 'New cloth brand “JAMBO” launch in Cambodia helped marketing and fashion show',
+    tag: ['fashion', 'SNS', 'model', 'idols', 'inflencer'],
+  },
+  {
+    id: 2,
+    category: 'Influencer marketing',
+    content: 'New cloth brand “JAMBO” launch in Cambodia helped marketing and fashion show',
+    tag: ['fashion', 'SNS', 'model', 'idols', 'inflencer'],
+  },
+  {
+    id: 3,
+    category: 'Influencer marketing',
+    content: 'New cloth brand “JAMBO” launch in Cambodia helped marketing and fashion show',
+    tag: ['fashion', 'SNS', 'model', 'idols', 'inflencer'],
+  },
+  {
+    id: 4,
+    category: 'Influencer marketing',
+    content: 'New cloth brand “JAMBO” launch in Cambodia helped marketing and fashion show',
+    tag: ['fashion', 'SNS', 'model', 'idols', 'inflencer'],
+  },
+  {
+    id: 5,
+    category: 'Influencer marketing',
+    content: 'New cloth brand “JAMBO” launch in Cambodia helped marketing and fashion show',
+    tag: ['fashion', 'SNS', 'model', 'idols', 'inflencer'],
+  },
+  {
+    id: 6,
+    category: 'Influencer marketing',
+    content: 'New cloth brand “JAMBO” launch in Cambodia helped marketing and fashion show',
+    tag: ['fashion', 'SNS', 'model', 'idols', 'inflencer'],
+  },
+  {
+    id: 7,
+    category: 'Influencer marketing',
+    content: 'New cloth brand “JAMBO” launch in Cambodia helped marketing and fashion show',
+    tag: ['fashion', 'SNS', 'model', 'idols', 'inflencer'],
+  },
+]
+</script>
+
+<style>
+
+</style>
