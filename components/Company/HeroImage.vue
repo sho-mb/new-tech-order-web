237a504b66fbba26227a887c49b91ea6682e1721
Kisho (Sho) Tamura <sho@mango-byte.com>
2024-05-30 13:31:26 +0700

    feat:create hero on company page

---
+++ components/Company/HeroImage.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div class="text-center heroBackground relative">
+      <img
+        src="/img/companyHero.png"
+        class="op-70"
+      >
+      <div class="absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center">
+        <div class="text-left">
+          <h1 class="text-7xl m-0">
+            BE ADVENTURES
+          </h1>
+          <div class="text-4xl font-600">
+            STAND OUT POP. BE YOURSELF.<br>YOU CAN BE SPECIAL.<br>WE GROW UP WITH KHMER <br>INFLUENCERS.
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+.heroBackground {
+  background: linear-gradient(0deg, #060405, #0D0407 , #17060C);
+}
+</style>
--- pages/company.vue
+++ pages/company.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    This is company page
+    <CompanyHeroImage />
   </div>
 </template>
 
