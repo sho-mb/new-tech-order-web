afe517a36e6f3431e7809b14a4eb2752c6e1baec
Kisho (Sho) Tamura <sho@mango-byte.com>
2024-05-31 08:52:26 +0700

    feat: create company form

---
+++ pages/contact.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="container py-20 bg~white max-w-[1240px]">
+    <ContactCompanyForm />
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+
+</style>
