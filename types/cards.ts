714083781af4a32053676b1a6973b7a6f6d26c50
Kisho (Sho) Tamura <sho@mango-byte.com>
2024-06-12 09:47:13 +0700

    feat: change texts

--- types/cards.ts
+++ types/cards.ts
@@ -1,7 +1,7 @@
 export const services = [
   {
     serviceTitle: 'Marketing Service',
-    serviceContent: 'Our influencer marketing service harnesses the power of<br/> influencers to convey the allure of products and services,<br/> attracting customer interest.',
+    serviceContent: 'Our marketing services connect people and support the growth and prosperity of businesses and communities.',
     cards: [
       {
         id: 1,
@@ -28,7 +28,7 @@ export const services = [
   },
   {
     serviceTitle: 'Production',
-    serviceContent: 'Our influencer marketing service harnesses the power of<br/> influencers to convey the allure of products and services,<br/> attracting customer interest.',
+    serviceContent: 'Our production services bring creative ideas to life, delivering high-quality content.',
     cards: [
       {
         id: 4,
@@ -56,22 +56,22 @@ export const servicesDetail = [
   },
   {
     text: 'SNS OPARATION',
-    content: 'Social Network Marketing Services(SNS Marketing) Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem error fugiat labore officiis vel dolorum accusamus iste ea quae commodi? <br /> Nemo in suscipit placeat fugit, ad officiis, atque dicta vel reiciendis voluptas eos blanditiis illum labore quos cumque odio sit facilis consectetur numquam ipsa aperiam.<br /> Ullam possimus tempore, illum dolorem accusamus ea optio et quasi commodi architecto id ut, atque quis dolores molestiae ab nisi.<br /> Placeat numquam vero error nulla id quae labore facilis minus eveniet neque reiciendis ab, earum libero velit accusamus accusantium deserunt fugiat inventore cupiditate assumenda minima aperiam doloremque aut!<br /> Facilis adipisci magni repellat debitis corrupti fuga.',
+    content: 'Through our SNS operation and nurturing services, we support an approach to the next generation. <br />We deploy strategies tailored to each client\'s needs, curating engaging content, fostering meaningful interactions with their audience, and meticulously managing social media platforms.<br /> Our goal is not only to enhance our clients\' visibility but also to cultivate loyal follower communities, fostering long-term growth and success. Leveraging our expertise, we adapt to the ever-changing landscape of social media, maximizing our impact and helping achieve business objectives.',
     url: '/img/card02.png',
   },
   {
     text: 'EVENT',
-    content: 'Social Network Marketing Services(SNS Marketing) Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem error fugiat labore officiis vel dolorum accusamus iste ea quae commodi? <br /> Nemo in suscipit placeat fugit, ad officiis, atque dicta vel reiciendis voluptas eos blanditiis illum labore quos cumque odio sit facilis consectetur numquam ipsa aperiam.<br /> Ullam possimus tempore, illum dolorem accusamus ea optio et quasi commodi architecto id ut, atque quis dolores molestiae ab nisi.<br /> Placeat numquam vero error nulla id quae labore facilis minus eveniet neque reiciendis ab, earum libero velit accusamus accusantium deserunt fugiat inventore cupiditate assumenda minima aperiam doloremque aut!<br /> Facilis adipisci magni repellat debitis corrupti fuga.',
+    content: 'Our event hosting service provides total support from planning to execution and management to realize our clients\' vision. <br />We offer customized events tailored to our clients\' needs and goals, focusing on delivering engaging experiences to participants. Our professional team brings creative ideas to life and ensures the smooth progress of events.<br /> Our goal is for clients to achieve their objectives through impressive events and receive high satisfaction from participants\' feedback.',
     url: '/img/card03.png',
   },
   {
     text: 'VIDEO PRODUCTION',
-    content: 'Social Network Marketing Services(SNS Marketing) Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem error fugiat labore officiis vel dolorum accusamus iste ea quae commodi? <br /> Nemo in suscipit placeat fugit, ad officiis, atque dicta vel reiciendis voluptas eos blanditiis illum labore quos cumque odio sit facilis consectetur numquam ipsa aperiam.<br /> Ullam possimus tempore, illum dolorem accusamus ea optio et quasi commodi architecto id ut, atque quis dolores molestiae ab nisi.<br /> Placeat numquam vero error nulla id quae labore facilis minus eveniet neque reiciendis ab, earum libero velit accusamus accusantium deserunt fugiat inventore cupiditate assumenda minima aperiam doloremque aut!<br /> Facilis adipisci magni repellat debitis corrupti fuga.',
+    content: 'Our video production service creates various video contents tailored to our clients\' needs. We cover a wide range of genres including advertising videos, promotional videos, product introductions, event documentaries, training videos, and more. <br />Our professional team utilizes a creative approach and the latest technology to deliver high-quality videos. <br />Our mission is to materialize our clients\' vision and produce videos that leave a strong impact on viewers.',
     url: '/img/card04.png',
   },
   {
     text: 'WEB DEVELOPMENT',
-    content: 'Social Network Marketing Services(SNS Marketing) Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem error fugiat labore officiis vel dolorum accusamus iste ea quae commodi? <br /> Nemo in suscipit placeat fugit, ad officiis, atque dicta vel reiciendis voluptas eos blanditiis illum labore quos cumque odio sit facilis consectetur numquam ipsa aperiam.<br /> Ullam possimus tempore, illum dolorem accusamus ea optio et quasi commodi architecto id ut, atque quis dolores molestiae ab nisi.<br /> Placeat numquam vero error nulla id quae labore facilis minus eveniet neque reiciendis ab, earum libero velit accusamus accusantium deserunt fugiat inventore cupiditate assumenda minima aperiam doloremque aut!<br /> Facilis adipisci magni repellat debitis corrupti fuga.',
+    content: 'Our website development service offers attractive and user-friendly websites tailored to our clients\' needs. <br />From design to development and content creation, we provide comprehensive support to enhance online presence. <br />Our professional team utilizes the latest technology and best practices to deliver effective web solutions that help achieve our clients\' business objectives',
     url: '/img/card05.png',
   },
 ]
