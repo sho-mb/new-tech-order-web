d5bdf7defbc46012652bfc4bb99f17d9ab124393
Kisho (Sho) Tamura <sho@mango-byte.com>
2024-06-13 08:16:27 +0700

    feat: create mobile on company

--- components/Company/VisionSection.vue
+++ components/Company/VisionSection.vue
@@ -1,13 +1,15 @@
 <template>
   <div>
     <div
-      class="backgroundTitle"
+      class="backgroundTitle1"
       data-aos="flip-up"
     >
       Vision
     </div>
+    <div class="border-b-solid bg-white mt-4 w-90px md:hidden" />
+
     <div
-      class="w-410px ml-33% pt-300px"
+      class="mt-50px w-410px ml-33% pt-300px md:block hidden"
       data-aos="fade-down"
       data-aos-easing="linear"
       data-aos-duration="1800"
@@ -22,6 +24,18 @@
         v-html="companyText.vision.contents"
       />
     </div>
+    <div
+      class="w-full mt-50px md:hidden"
+    >
+      <div
+        class="text-2xl font-bold"
+        v-html="companyText.vision.title"
+      />
+      <div
+        class="mt-4 w-90%"
+        v-html="companyText.vision.contents"
+      />
+    </div>
   </div>
 </template>
 
@@ -30,11 +44,21 @@ import { companyText } from '~/types/company'
 </script>
 
 <style>
-.backgroundTitle {
-  position: absolute;
-  opacity: 0.15;
-  font-size: 394px;
+.backgroundTitle1 {
+  width: fit-content;
+  margin-top: 32px;
+  opacity: 1;
+  font-size: 28px;
   font-weight: bold;
-  left: -80px;
+}
+
+@media (min-width: 768px) {
+  .backgroundTitle1 {
+    position: absolute;
+    opacity: 0.15;
+    font-size: 394px;
+    font-weight: bold;
+    left: -80px;
+  }
 }
 </style>
