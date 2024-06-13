3872db6672c484ab37e949c9a33b2b6b804c939c
Kisho (Sho) Tamura <sho@mango-byte.com>
2024-06-13 13:36:44 +0700

    feat: change image hero and add padding top on hero

--- components/Top/HeroImage.vue
+++ components/Top/HeroImage.vue
@@ -1,8 +1,13 @@
 <template>
-  <div class="pt-10 w-full md:pt-0">
+  <div class="w-full md:pt-16">
     <img
       src="/img/hero.png"
-      class="w-full"
+      class="w-full hidden md:block"
+    >
+    <img
+      src="/img/companyHero.png"
+      class="w-full h-360px md:hidden"
+      style="object-fit: cover;"
     >
   </div>
 </template>
