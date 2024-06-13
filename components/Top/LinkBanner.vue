444b731701f6084e687008591e7c83aa1b7ebc64
Kisho (Sho) Tamura <sho@mango-byte.com>
2024-06-13 14:08:52 +0700

    chore: add line on bannaer and make hover

--- components/Top/LinkBanner.vue
+++ components/Top/LinkBanner.vue
@@ -1,11 +1,12 @@
 <template>
   <div
     data-aos="flip-left"
-    class="w-full h-full rounded-3xl overflow-hidden"
+    class="w-full rounded-3xl overflow-hidden border-solid relative"
   >
     <NuxtLink to="https://www.instagram.com/p/C68-rq2hlKF/?img_index=1">
+      <div class="absolute bg-black w-full h-full opacity-0 hover:opacity-20" />
       <img
-        class="w-full object-cover h-full"
+        class="w-full object-cover "
         src="/img/banner.jpg"
       >
     </NuxtLink>
